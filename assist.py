2733e81884515704360c3b97e33601dc11f7699e
Lidia <lidiahryma22@gmail.com>
2023-07-07 14:47:04 +0300

    third commit

--- assist.py
+++ assist.py
@@ -1,27 +1,60 @@
+contacts ={}
+
 def handle_errors(func):
-    def wrapper(*args):
+    def wrapper(*args,**kwargs):
         try:
-            return func(*args)
-        except Exception as e:
-            return f"Error: {str(e)}"
+            return func(*args,**kwargs)
+        except KeyError:
+            return "Contact not found"
+        except ValueError:
+            return "Invalid input. Please enter a valid name and phone number"
+        except IndexError:
+            return "Invalid input. please try again"
     return wrapper
 
 
 @handle_errors
 def add(*args):
-    # # if len(*args) < 2:
-    #     return "Error"
-    name = args[0]
-    phone = args[1]
+    name,phone = args
+    contacts[name] = phone
     return f"Add success {name}, {phone}"
 
 
 @handle_errors
-def greeting(text):
-    if text in ["hello","good morning"]:
-        return "How can I help you?"
+def greeting(*text):
+    return "How can I help you?"
+    
+@handle_errors
+def exit_command (*args):
+    return "See you soon"             
+
+@handle_errors
+def change (*args):
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return "Phone updated successfully"
     else:
-        return "See you soon"                 #Не знаю,як змінити відповідь "Hello" - "How can I helpl you?"
+        return "Contact not found"
+    
+@handle_errors
+def phone(*args):
+    name = args[0]
+    if name in contacts:
+        return f"Phone number for{name} is {contacts[name]}"
+    else:
+        return f" Contact not found"
+    
+@handle_errors
+def show_all(*args):
+    if contacts:
+        all_cintacts = ""
+        for name, phone in contacts.items():
+            all_cintacts+=f"{name}:{phone} "
+        return all_cintacts
+    else:
+        return "Contact not found"   
+
 
 @handle_errors
 def no_command(*args):
@@ -30,26 +63,31 @@ def no_command(*args):
 
 def parser(text: str) -> tuple[callable, tuple[str] | None]:
     if text.startswith('add'):
-        return add, text.replace("add", "").strip().split()
+        return add, tuple(text.replace("add", "").strip().split())
+    elif text.startswith("change"):
+        return change, tuple(text.replace('change', "").strip().split())
+    elif text.startswith("phone"):
+        return phone, text.replace ("phone", "").strip()
+    elif text.startswith ("show all"):
+        return show_all,tuple(text.replace("show all","").strip().split())
     elif text == 'hello':
-        return greeting, (text,)
+        return greeting, text
     elif text =='good bye':
-        return greeting, (text, )
-    else:
-        return no_command, None
-
+        return exit_command,text
+    return no_command()
 
 def main():
     while True:
         user_input = input(">>>>")
         command, data = parser(user_input)
         if command is None:
-            print("See you soon!")
+            print("See you soon")
             break
         result = command(data)
-        if result is not None:
+        if result:
             print(result)
-
+        if command == exit_command:
+            break
 
 if __name__ == "__main__":
     main()
